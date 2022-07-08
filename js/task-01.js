d796d6db0a4eff385c94bc7859b256fd4c75a376
MickKorogod <korogod1995@gmail.com>
2022-07-09 00:35:53 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll('#categories > li.item');
+console.log(`Number of categories:${categories.length}`);
+
+const items = document.querySelectorAll('.item');
+Array.prototype.forEach.call(items, (element) => {
+    const title = element.querySelector('h2').innerHTML;
+    const itemsLength = element.querySelectorAll('li').length;
+    console.log(`Category: ${title} / Elements: ${itemsLength}`);
+});
