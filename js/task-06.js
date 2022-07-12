70c3480cb66c3556d9e28785c0f7c4f26993a18b
MickKorogod <korogod1995@gmail.com>
2022-07-12 23:39:25 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,14 @@ const validLength = parseInt(input.getAttribute("data-length"))
  
 function focusOff(e) {
     if (validLength === e.currentTarget.value.length) {
-     input.classList.add("valid")   
+        input.classList.add("valid") 
+        input.classList.remove("invalid")  
         
     }
-    else { input.classList.add('invalid')}
+    else {
+        input.classList.add('invalid')
+        input.classList.remove("valid")
+    }
 }
  
 input.addEventListener('blur',focusOff)
